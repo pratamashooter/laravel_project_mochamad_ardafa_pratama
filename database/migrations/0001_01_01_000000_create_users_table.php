b910ebee8e4a371135fbf2072804c4e4312e48cb
Ardafa Pratama <ardafapratama123@gmail.com>
2025-11-22 03:19:47 +0700

    feat(auth): implement authentication foundation

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('alamat', 255);
-            $table->string('telepon', 20);
+            $table->string('alamat', 255)->nullable();
+            $table->string('telepon', 20)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
